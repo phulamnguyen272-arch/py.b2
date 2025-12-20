ef2d09a58398cc78b9c9348d68debecba8c2de0c
mc_better_than_mu <nguyenkhaclam272@gmail.com>
2025-12-20 10:16:20 +0700

    btvnb2.py

---
+++ btvnb2.py
@@ -0,0 +1,10 @@
+print("Tôi yêu python")
+print(f"đáp án câu 10+5 là: {10+5}")
+print(f"đáp án câu 10-5 là: {10-5}")
+print(f"đáp án câu 10*5 là: {10*5}")
+print(f"đáp án câu 10/3 là: {10/3}")
+print(f"số dư câu 10%3 là: {10%3}")
+coin_a=3
+coin_b=5
+total_coin = coin_a + coin_b
+print(f"tổng coin_a và coin_ b là: {total_coin}")

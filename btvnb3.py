c1aeb9f47247ba779b8bad453227700ed42160cf
mc_better_than_mu <nguyenkhaclam272@gmail.com>
2025-12-27 10:43:37 +0700

    btvnb3.py

---
+++ btvnb3.py
@@ -0,0 +1,21 @@
+#bài 1
+# while True:
+#     n = int(input("nhập n"))
+#     if n % 2 == 0:
+#         print("n là số chẵn")
+#     else:
+#         print("n là số lẻ")
+#bài 2        
+# while True:
+#     điểm_học_sinh = int(input("nhập điểm học sinh"))
+#     tên_học_sinh = input("nhập tên học sinh")
+#     if điểm_học_sinh >=8:
+#         print(f"học sinh có số điểm<=8 là {tên_học_sinh}")
+
+#bài 3
+while True:
+    n = int(input("hãy nhập số nguyên:"))
+    if n<0:
+        print("vui lòng nhập số không âm")
+    else:
+        print(n**2)
